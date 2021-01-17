f12723e65f694a4dc221e3cad1368e0664663fe3
dlrandy <1208484996@qq.com>
2021-01-17 23:01:48 +0800

    feat:stash

---
+++ design-patterns/creational/singleton/singleton-dependencies/db.mjs
@@ -0,0 +1,7 @@
+/* eslint-disable no-underscore-dangle */
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import sqlite3 from 'sqlite3';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+export const db = new sqlite3.Database(join(__dirname, 'data.sqlite'));
